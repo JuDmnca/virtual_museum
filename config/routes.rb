9205b4f05f434a593165299e38687509b58544f8
JuDmnca <julie.demendonca@mmibordeaux.com>
2019-03-07 10:24:43 +0100

    root devient colors

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :artists
   resources :colors
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "colors#{index}"
 end
+
